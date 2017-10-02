527cb44fe31bade86bcb4bbd7bc40cc6b1982907
shaudai <shaudai0402@hotmail.com>
2017-10-02 22:50:44 +0800

    get the text content from a html component

--- app.js
+++ app.js
@@ -19,3 +19,6 @@ dice = Math.floor(Math.random() * 6) +1;
 
 document.querySelector('#current-' + activePlayer).textContent = dice;
 //document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+
+var x = document.querySelector('#score-0').textContent;
+console.log(x);
