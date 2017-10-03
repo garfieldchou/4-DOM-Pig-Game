8ff6fd36036bbbdd616f4d894f0a42ece7029857
shaudai <shaudai0402@hotmail.com>
2017-10-03 20:42:57 +0800

    use getElementById() to set the content of scores as 0

--- app.js
+++ app.js
@@ -17,6 +17,11 @@ activePlayer = 0;
 
 document.querySelector('.dice').style.display = 'none';
 
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
 document.querySelector('.btn-roll').addEventListener('click', function(){
     
     // 1. Random number
