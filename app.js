056845e60470cae27a3cc1c6b1b533d7f133d2d0
shaudai <shaudai0402@hotmail.com>
2017-10-04 22:47:03 +0800

    hide the dice and add winner class to the corresponding player

--- app.js
+++ app.js
@@ -54,6 +54,9 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
     // Check if player won the game
     if (scores[activePlayer] >= 20) {
         document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
     } else {
         // Next Player
         nextPlayer();        
