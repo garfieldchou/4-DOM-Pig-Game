d19ca5aa4f9460c31a15fd7c143c5d79e0b5894b
shaudai <shaudai0402@hotmail.com>
2017-10-04 22:31:34 +0800

    update the score while hold

--- app.js
+++ app.js
@@ -56,6 +56,16 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
     
 });
 
+document.querySelector('.btn-hold').addEventListener('click', function() {
+    // Add CURRENT score to GLOBAL score
+    scores[activePlayer] += roundScore;
+    
+    // Update the UI
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+    
+    // Check if player won the game
+});
+
 //document.querySelector('#current-' + activePlayer).textContent = dice;
 //document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
 //var x = document.querySelector('#score-0').textContent;
